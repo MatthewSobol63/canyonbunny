676f83cfcb1af5aceb02e2f293f02a273b2f2be6
shantejo <rathaloskiller@web.de>
2015-07-03 00:32:17 +0200

    added texturepacker

--- desktop/src/cpm/packtpub/libgdx/canyonbunny/desktop/DesktopLauncher.java
+++ desktop/src/cpm/packtpub/libgdx/canyonbunny/desktop/DesktopLauncher.java
@@ -2,11 +2,30 @@ package cpm.packtpub.libgdx.canyonbunny.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+
 import cpm.packtpub.libgdx.canyonbunny.CanyonBunnyMain;
 
 public class DesktopLauncher {
+	private static boolean rebuildAtlas = true;
+	private static boolean drawDebugOutline = true;
+
 	public static void main (String[] arg) {
+		if (rebuildAtlas) {
+			System.out.println(System.getProperty("user.dir"));
+			TexturePacker.Settings settings = new TexturePacker.Settings();
+			settings.maxWidth = 1024;
+			settings.maxHeight = 1024;
+			settings.duplicatePadding = false;
+			settings.debug = drawDebugOutline;
+			TexturePacker.process(settings, "../../desktop/assets-raw/images",
+					"images", "canyonbunny.pack");
+		}
+
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Canyon Bunny";
+		config.width = 800;
+		config.height = 480;
 		new LwjglApplication(new CanyonBunnyMain(), config);
 	}
 }
